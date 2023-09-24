243b616dbf24065b42752230b662beecacd4ce68
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-24 17:17:54 +0300

    Done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,31 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def __init__(self, url):
+        self.url = url
 
-    response = requests.get(URL)
-    return response.content
+    def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
 
+    def program_school(self):
+        programs_list = []
+        programs = json.loads(self.get_response_body())
+        for program in programs:
+            programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+        return programs_list
+
+if __name__ == "__main__":
+    # Specify the URL for the NYC Open Data API
+    nyc_api_url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+    # Create an instance of GetPrograms with the API URL
+    programs = GetPrograms(nyc_api_url)
+
+    # Get and print the list of program schools
+    programs_schools = programs.program_school()
+    for school in set(programs_schools):
+        print(school)
